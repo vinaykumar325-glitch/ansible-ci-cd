387c77bb4e5ae0ee99bd81d6a149792f92357383
c vinay kumar <chilamalavinaykumar@gmail.com>
2024-08-08 22:10:56 +0530

    Update main.tf by vinnu

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "aws_security_group" "vpc-web" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
